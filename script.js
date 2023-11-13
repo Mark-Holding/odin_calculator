ddcb33163b8076b8602f20fa25add8166ddfeadd
Mark H <mark-holding@hotmail.co.uk>
2023-11-13 16:29:07 +0000

    link = key and operate function to give result.

--- script.js
+++ script.js
@@ -11,14 +11,28 @@ document.addEventListener('keydown', function(event) {
     } else if (event.key === '=') {
         calcArray = pressedKeys.split(/([+\-*\/])/).filter(Boolean);
         // Remove the following line to keep the display value after processing the calculation
-        display.value = '';
+        display.value = operate(calcArray[0], calcArray[2], calcArray[1])
+
 
         console.log('Pressed Keys:', pressedKeys);
         console.log('Calc Array:', calcArray);
     }
 });
 
-
+function operate(a, b, c){
+    a = Number(a);
+    b = Number(b);
+    
+    if(c === '+') {
+        return add(a, b);
+    } else if (c === '-'){
+        return subtract(a, b);
+    } else if (c === '*'){
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
+    }
+}
 
 
 let num1;
@@ -41,15 +55,5 @@ function divide(a, b) {
     return a / b;
 };
 
-function operate(a, b, c){
-    if(c === '+') {
-        return add(a, b);
-    } else if (c === '-'){
-        return subtract(a, b);
-    } else if (c === '*'){
-        return multiply(a, b);
-    } else {
-        return divide(a, b);
-    }
-}
+
 
